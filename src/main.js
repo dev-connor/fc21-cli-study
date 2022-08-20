53b9707ff278bd62a5e80977f9926e5c312bce83
Connor <kor.connor@gmail.com>
2022-08-20 16:36:09 +0900

    토큰으로 깃허브 접근하기

--- src/main.js
+++ src/main.js
@@ -3,20 +3,31 @@
 require('dotenv').config()
 
 const { GITHUB_ACCESS_TOKEN } = process.env
-console.log('TOKEN: ', GITHUB_ACCESS_TOKEN)
 
 const { program } = require('commander')
-const fs = require('fs')
+const { Octokit } = require('octokit')
 
 program.version('0.0.1')
+
+const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN })
+
+program
+  .command('me')
+  .description('Check my profile')
+  .action(async () => {
+    const {
+      data: { login },
+    } = await octokit.rest.users.getAuthenticated()
+    console.log("Hello, %s", login)
+  })
 
 program
   .command('list-bugs')
   .description('List issues with bug label')
   .action(async () => {
     console.log('Before readFile...')
-    const result = await fs.promises.readFile('.prettierrc')
-    console.log('readFile result: ', result)
+    // const result = await fs.promises.readFile('.prettierrc')
+    // console.log('readFile result: ', result)
     console.log('List bugs!');
   })
 
